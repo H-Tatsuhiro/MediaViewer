cfe7180443b839705a24e7ce9b6cdcf3400edb89
H.Tatsuhiro <admin@h-tatsu.com>
2020-10-01 19:18:29 +0900

    コマンドラインにて読み込みファイルと保存先を指定した上でファイルを保存

---
+++ workspace/test_proj/src/main.rs
@@ -0,0 +1,7 @@
+extern crate test_proj;
+
+use test_proj::pic;
+
+fn main() {
+    pic::img_save();
+}
--- workspace/test_proj/src/pic.rs
+++ workspace/test_proj/src/pic.rs
@@ -1,14 +1,23 @@
 extern crate image;
 
-pub fn path_read() {
+fn path_read() -> String {
     let mut args = std::env::args();
     args.next();
     let path = match args.next() {
         None => {
             println!("Not Found");
-            return
+            return String::from("null")
         },
         Some(s) => s,
     };
-    println!("{}", path);
+
+    return path;
+}
+
+pub fn img_save() {
+    let args: Vec<String> = std::env::args().collect();
+    let path = &args[1];
+    let img = image::open(&path).unwrap();
+    let path_save = &args[2];
+    img.save(&path_save).unwrap();
 }
